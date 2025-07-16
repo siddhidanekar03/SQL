8c833d51f465810bf68dfac434f60c8b3912c263
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-07-16 21:38:53 +0530

    Add files via upload

---
+++ CREATE.sql
@@ -0,0 +1,6 @@
+-- USE HARRY;
+-- INSERT INTO users  VALUES(1, 'ALICE', 'harry@gmail.com', 'Male', '2025-07-03',default);
+INSERT INTO users  VALUES(2, 'RLICE', 'rlice@gmail.com', 'Male', '2025-07-03',default);
+INSERT INTO users  VALUES(3, 'SLICE', 'slice@gmail.com', 'Female', '2025-07-09',default);
+INSERT INTO users  VALUES(4, 'MLICE', 'mlice@gmail.com', 'Female', '2025-07-08',default);
+SELECT * FROM users;
---
+++ DAY1.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE HARRY;
+USE HARRY;
+CREATE TABLE users1 (
+id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(100) UNIQUE NOT NULL,
+  gender ENUM('Male', 'Female', 'Other'),
+  date_of_birth DATE,
+  salary DECIMAL(10, 2),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+ SELECT * FROM users;
+ 
