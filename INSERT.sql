915d5c6b0a9338ee7468eb90436f1c5d37249584
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-07-17 22:39:30 +0530

    Add files via upload

---
+++ INSERT.sql
@@ -0,0 +1,11 @@
+USE HARRY;
+-- SELECT * FROM users1 WHERE salary BETWEEN 10000 AND 110000 ;
+-- SELECT * FROM users1 WHERE gender IN ('Male', 'Other');
+-- SELECT * FROM users1 ORDER BY date_of_birth ASC;
+-- SELECT * FROM users1 ORDER BY name DESC;
+-- SELECT * FROM users1 LIMIT 5;
+-- SELECT * FROM users1 LIMIT 10 OFFSET 5;
+-- SELECT * FROM users1 ORDER BY created_at DESC LIMIT 10;
+-- SELECT * FROM users1 WHERE salary > 60000 ORDER BY created_at DESC LIMIT 5;
+-- SELECT * FROM users1 ORDER BY salary DESC;-- 
+SELECT * FROM users1 WHERE salary BETWEEN 50000 AND 70000;
